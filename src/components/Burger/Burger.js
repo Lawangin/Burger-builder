b038f4a0012369347a4906b5dc3a04178b787b4a
Lawangin Khan <lawangin.khan@gmail.com>
2019-10-03 21:24:27 -0400

    Component tree added with burderbuilder and modal.

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import BurgerIngredient from '../Burger/BurgerIngredient/BurgerIngredient';
+import classes from './Burger.module.css';
+
+const burger = (props) => {
+    // Object.keys returns an array of the key for the values in object and map loops through that array
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />;
+        });
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el);
+    }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please add some ingredients!</p>
+    }
+
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );
+};
+
+export default burger;
